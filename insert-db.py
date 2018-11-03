5c4d1037ff40039cb8c58202ccbfef399a111c60
chandracanth <chandracanth95@gmail.com>
2018-11-03 23:54:51 +0530

    insert path in database

---
+++ insert-db.py
@@ -0,0 +1,33 @@
+import mysql.connector as mysql #mysql-connector
+import sys
+
+db= mysql.connect(user="root",password="chandracanth_5495",host="localhost",database="dsaa")
+mycursor = db.cursor()
+
+"""
+ ---- Run dsaa-schema.sql ------
+ command : source dsaa-schema.sql
+create database if not exists dsaa;
+use dsaa;
+create table test (id int(10) AUTO_INCREMENT primary key,address varchar(255), UNIQUE(address) );
+"""
+
+"""
+sql = "INSERT INTO test (address,just) VALUES (%s ,%s)"
+val=[('/home/chandra/dsaa-project/images/OS-1.jpg','1'),('/home/chandra/dsaa-project/images/OS-2.jpg','1')]
+mycursor.executemany(sql,val)
+db.commit()
+"""
+mycursor.execute("select * from test")
+
+data = mycursor.fetchall()
+
+a=[]
+for i in data:
+	a.append(i[0])
+	print i
+print a
+for i in range(len(a)):
+	print i," - ",a[i]
+mycursor.close()
+db.close()
